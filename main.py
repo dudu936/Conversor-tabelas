8693d4f47d2d8674a428c1e6983311d8bc237681
Eduardo <73761319+dudu936@users.noreply.github.com>
2023-05-19 19:05:22 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-pdf_file   =   "RELATÓRIO-17-05-2023.pdf"
-xlsx_file = pdf_file[:-3] + "xlsx"
-rows=["C.Pagar: Nr. Lanc.","COMPRA MERCADORIA","Dt. Entrada","dias","Código Conta","Prazo Indice"]
-separator=["Embalagem UN Qt. P.Unit. Custo Fin.Ant.","Filial Embalagem UN Qt.","UN Qt. P.Unit. Custo Fin.Ant.","Filial Embalagem UN","Embalagem UN Qt.","UN Qt. P.Unit.","Qt. P.Unit. Custo Fin.Ant.","P.Unit. Custo Fin.Ant. Custo Fin.Atual","Custo Fin.Ant. Custo Fin.Atual Cód. Fiscal","Custo Fin.Atual Cód. Fiscal Cód. Oper.","Filial Embalagem","Embalagem UN","UN Qt.","Qt. P.Unit. Custo","P.Unit. Custo Fin.Ant.","Custo Fin.Ant. Custo Fin.Atual","Custo Fin.Atual Cód. Fiscal","Cód. Fiscal Cód. Oper."]
-columns=["Filial","Embalagem","Custo Fin.Atual","Custo","Fin.Ant.","Fin.Atual","Cód. Fiscal","Cód.","Fiscal","Cód. Oper.","NaN","nan"]
-
-main(pdf_file=pdf_file,xlsx_file=xlsx_file,index=1,rows=rows,columns=columns,separator=separator)
